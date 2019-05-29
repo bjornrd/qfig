0ea6291829676fdecb81cd17e858e080752c8c14
Bjørn Rudi Dahl <bjorn.rudi.dahl@biimultrasound.com>
2019-05-29 13:35:03 +0200

    First Commit of Qt Figure

---
+++ QFig.cpp
@@ -0,0 +1,125 @@
+/****************************************************************************
+ * Copyright (C) 2019 Bjørn Rudi Dahl
+ *
+ * Written by: bjornrd
+ * 27/5/2019
+ * me_brd@hotmail.com
+ ****************************************************************************/
+#include "QFig.h"
+#include <QPixmap>
+#include <QGraphicsScene>
+#include <QGraphicsPixmapItem>
+#include <QApplication>
+
+namespace qf{
+
+QGraphicsView* setUpNewGraphicsView(QPixmap pixmap);
+
+
+
+QFig::QFig(QWidget* parent) :
+    QMainWindow(parent),
+    _figureView(nullptr),
+    _resizeOpt(FitOnResize),
+    _aspectRatioMode(KeepAspectRatio)
+{
+    // Make the figure windows close when the main app windows close
+    this->setAttribute(Qt::WA_QuitOnClose, false);
+}
+
+QFig::~QFig()
+{
+    if(_figureView != nullptr)
+        delete _figureView;
+}
+
+void QFig::close()
+{
+    QFig::~QFig();
+}
+
+QGraphicsView* QFig::view()
+{
+    return _figureView;
+}
+
+void QFig::setView(QGraphicsView *newView)
+{
+    _figureView = newView;
+    this->setCentralWidget(_figureView);
+}
+
+FigureResizeOption QFig::resizeOption()
+{
+    return _resizeOpt;
+}
+
+void QFig::setResizeOption(FigureResizeOption opt)
+{
+    _resizeOpt = opt;
+}
+
+void QFig::resizeEvent(QResizeEvent *event)
+{
+    QMainWindow::resizeEvent(event);
+
+    if(_resizeOpt == FitOnResize)
+        _figureView->fitInView(_figureView->items().at(0)->boundingRect(), static_cast<Qt::AspectRatioMode>(_aspectRatioMode));
+}
+
+AspectRatioMode QFig::aspectRatioMode() const
+{
+    return _aspectRatioMode;
+}
+
+void QFig::setAspectRatioMode(const AspectRatioMode &aspectRatioMode)
+{
+    _aspectRatioMode = aspectRatioMode;
+}
+
+QFig* imshow(QPixmap pixmap, QString title, ImageOptions opt, QWidget* parent)
+{
+    auto view = setUpNewGraphicsView(pixmap);
+
+    QFig* figure = new QFig(parent);
+
+    figure->setResizeOption(opt.figureResizeOption());
+    figure->setWindowTitle(title);
+    figure->setView(view);
+    figure->show();
+
+    return figure;
+}
+
+QFig* imshow(QPixmap pixmap, ImageOptions opt, QWidget* parent)
+{
+    return imshow(pixmap, "", opt, parent);
+}
+
+QFig* imshow(QImage image, ImageOptions opt, QWidget* parent)
+{
+    return imshow(QPixmap::fromImage(image), "", opt, parent);
+}
+
+QFig* imshow(QImage image, QString title, ImageOptions opt, QWidget* parent)
+{
+    return imshow(QPixmap::fromImage(image), title, opt, parent);
+}
+
+QGraphicsView* setUpNewGraphicsView(QPixmap pixmap)
+{
+    QGraphicsView*       view   = new QGraphicsView();
+    QGraphicsScene*      scene  = new QGraphicsScene(view);
+    QGraphicsPixmapItem* item   = new QGraphicsPixmapItem();
+
+    view->setScene(scene);
+    scene->addItem(item);
+    item->setPixmap(pixmap);
+    scene->setSceneRect(item->boundingRect());
+
+    view->fitInView(item, Qt::KeepAspectRatio);
+
+    return view;
+}
+
+} // namespace qf
---
+++ QFig.h
@@ -0,0 +1,98 @@
+/****************************************************************************
+ * Copyright (C) 2019 Bjørn Rudi Dahl
+ *
+ * Written by: bjornrd
+ * 27/5/2019
+ * me_brd@hotmail.com
+ ****************************************************************************/
+#ifndef QFIG_H
+#define QFIG_H
+
+#include <QMainWindow>
+#include <QGraphicsView>
+
+#if defined(QTFIG_LIBRARY)
+#  define QTFIGSHARED_EXPORT Q_DECL_EXPORT
+#else
+#  define QTFIGSHARED_EXPORT Q_DECL_IMPORT
+#endif
+
+namespace qf{
+
+enum QTFIGSHARED_EXPORT FigureResizeOption{
+    FitOnResize,
+    NoFitOnResize
+};
+
+enum QTFIGSHARED_EXPORT FigureOptions {
+    ZoomOnScroll,
+    GainOnScroll
+};
+
+enum QTFIGSHARED_EXPORT AspectRatioMode {
+    KeepAspectRatio   = Qt::AspectRatioMode::KeepAspectRatio,
+    IgnoreAspectRatio = Qt::AspectRatioMode::IgnoreAspectRatio
+};
+
+struct ImageOptions {
+    public:
+        ImageOptions(FigureResizeOption resizeOption    = FigureResizeOption::FitOnResize,
+                     FigureOptions      figureOption    = FigureOptions::ZoomOnScroll,
+                     AspectRatioMode    aspectRatioMode = AspectRatioMode::KeepAspectRatio) :
+            _figureResizeOption(resizeOption),
+            _figureOptions(figureOption),
+            _aspectRatioMode(aspectRatioMode)
+        {}
+
+        FigureResizeOption figureResizeOption() const {return _figureResizeOption;}
+        void setFigureResizeOption(const FigureResizeOption &opt){_figureResizeOption = opt;}
+
+        FigureOptions figureOptions() const {return _figureOptions;}
+        void setFigureOptions(const FigureOptions &figureOptions) {_figureOptions = figureOptions;}
+
+        AspectRatioMode aspectRatioMode() const {return _aspectRatioMode;}
+        void setAspectRatioMode(const AspectRatioMode &aspectRatioMode) {_aspectRatioMode = aspectRatioMode;}
+
+    private:
+        FigureResizeOption  _figureResizeOption  = FitOnResize;
+        FigureOptions       _figureOptions       = ZoomOnScroll;
+        AspectRatioMode     _aspectRatioMode     = KeepAspectRatio;
+};
+
+class QTFIGSHARED_EXPORT QFig : public QMainWindow
+{
+        Q_OBJECT
+    public:
+         QFig(QWidget* parent = nullptr);
+        ~QFig();
+
+         void close();
+
+         QGraphicsView* view();
+         void setView(QGraphicsView* newView);
+
+         qf::FigureResizeOption resizeOption();
+         void setResizeOption(qf::FigureResizeOption opt);
+
+         AspectRatioMode aspectRatioMode() const;
+         void setAspectRatioMode(const AspectRatioMode &aspectRatioMode);
+
+    protected:
+         void resizeEvent(QResizeEvent* event);
+
+    private:
+        QGraphicsView*      _figureView;
+        FigureResizeOption  _resizeOpt;
+        AspectRatioMode     _aspectRatioMode;
+};
+
+const static ImageOptions defaultImageOptions = ImageOptions();
+
+QTFIGSHARED_EXPORT QFig* imshow(QPixmap pixmap,                 ImageOptions opt = ImageOptions(), QWidget* parent = nullptr);
+QTFIGSHARED_EXPORT QFig* imshow(QPixmap pixmap, QString title,  ImageOptions opt = ImageOptions(), QWidget* parent = nullptr);
+QTFIGSHARED_EXPORT QFig* imshow(QImage image,                   ImageOptions opt = ImageOptions(), QWidget* parent = nullptr);
+QTFIGSHARED_EXPORT QFig* imshow(QImage image,   QString title,  ImageOptions opt = ImageOptions(), QWidget* parent = nullptr);
+
+
+}
+#endif // QFIG_H
